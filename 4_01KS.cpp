a1974c5f67ac7576f13f36f572ca8f26238d10f7
PankajGolhar <155148709+PankajGolhar@users.noreply.github.com>
2024-11-06 08:32:58 +0530

    Add files via upload

---
+++ 4_01KS.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+
+int knapsack(int w, const vector<int>& weights,const vector<int>& profits,int n)
+{
+    vector<vector<int>> dp(n+1,vector<int>(w+1,0));
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<=w;j++)
+        {
+            if(weights[i-1]<=j)
+            {
+                dp[i][j]=max(profits[i-1]+dp[i-1][j-weights[i-1]],dp[i-1][j]);
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][w];
+}
+
+
+int main(){
+    int n, W;
+    
+    cout << "Enter the number of items: ";
+    cin >> n;
+    
+    cout << "Enter the maximum weight capacity of the knapsack: ";
+    cin >> W;
+
+    vector<int> profits(n), weights(n);
+
+    cout << "Enter the profit and weight of each item:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Item " << i + 1 << " profit and weight: ";
+         cin >> profits[i] >> weights[i];
+    }
+
+    int maxProfit = knapsack(W, weights, profits, n);
+    cout << "Maximum profit: " << maxProfit << endl;
+
+    return 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Enter the number of items: 4
+// Enter the maximum weight capacity of the knapsack: 8
+// Enter the profit and weight of each item:
+// Item 1 profit and weight: 2
+// 3
+// Item 2 profit and weight: 3
+// 4
+// Item 3 profit and weight: 1
+// 6
+// Item 4 profit and weight: 4
+// 5
+// Maximum profit: 6
